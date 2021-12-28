c8a071432b42f23b5e32d8cc22cff91ee5eae1a0
ckie <git-525ff67@ckie.dev>
2021-12-28 03:32:29 +0200

    fix runtime errors when running nix build

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import CookiecordClient from "cookiecord";
+import BasicModule from "./modules/basic";
 import { botAdmins, token } from "./env";
+import ChallengeModule from "./modules/challenge";
 
 const client = new CookiecordClient({
   botAdmins,
   prefix: "al "
 });
 
-client.loadModulesFromFolder("src/modules");
-client.reloadModulesFromFolder("src/modules");
+if (process.env.LIVE_RELOAD) {
+  client.loadModulesFromFolder("src/modules");
+  client.reloadModulesFromFolder("src/modules");
+} else {
+  client.registerModule(BasicModule);
+  client.registerModule(ChallengeModule);
+}
+
 client.login(token);
 client.on("ready", () => console.log(`Logged in as ${client.user?.tag}`));
